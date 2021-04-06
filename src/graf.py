5bff452b74dbbe5b0c0c47afa652800da8bfb284
afifahfq <afifahfq@gmail.com>
2021-04-06 23:36:49 +0700

    edit simpul

--- src/graf.py
+++ src/graf.py
@@ -1,15 +1,15 @@
 def readfile(namafile):
-    global graf, grafsimpul, banyaksimpul, bobot;
-    file = open("./test/"+ namafile + ".txt", "r")
-    banyaksimpul = int(file.readline());
-    #print("Banyak Simpul : ", banyaksimpul)
+    global graf, grafsimpul, banyaksimpul, bobot
+    file = open("../test/"+ namafile + ".txt", "r")
+    banyaksimpul = int(file.readline())
+    print("Banyak Simpul : ", banyaksimpul)
     grafsimpul = file.readline()
     graf = file.readlines()
-    #print(grafsimpul);
-    #print(graf)
+    #print("grafsimpul:", grafsimpul)
+    #print("graf:", graf)
 
 def ceksimpul():
-    global simpul;
+    global simpul
     '''
     print(simpul)
     output : ['jawa', 'sumatra', 'bali', 'papua', 'sulawesi', 'mamuju', 'ternate', 'kalimantan']
@@ -26,9 +26,16 @@ def ceksimpul():
                 simpul += [huruf]
             elif (grafsimpul[i][j] != ','):
                 huruf += grafsimpul[i][j]
+    splitsimpul()
+
+def splitsimpul():
+    newsimpul = [char for char in simpul[0]]
+    simpul.clear()
+    simpul.extend(newsimpul)
+    #print("newsimpul:", newsimpul)
 
 def isimatriks():
-    global hasil;
+    global hasil
     '''
     print(hasil) -> matriks
     output : [['-1', '2', '-1', '-1', '-1', '1', '-1', '-1'], ['2', '-1', '2', '2', '4', '-1', '-1', '-1'], ['-1', '2', '-1', '-1', '3', '-1', '-1', '1'], ['-1', '2', '-1', '-1', '4', '3', '-1', '-1'], ['-1', '4', '3', '4', '-1', '-1', '7', '-1'], ['1', '-1', '-1', '3', '-1', '-1', '5', '-1'], ['-1', '-1', '-1', '-1', '7', '5', '-1', '6'], ['-1', '-1', '1', '-1', '-1', '-1', '6', '-1']]
@@ -63,11 +70,12 @@ def grafindict():
     for i in range(banyaksimpul):
         akhir += [(simpul[i], hasil[i])]
         #akhir += [(simpul[i], i)]
-        
+    
+    #print("Akhir:", akhir)
     grafberbobot = dict(akhir)
 
 def grafberbobotberpasangan():
-    global g;
+    global g
     '''
     print(g)
     Output : [('jawa', 'sumatra', 2), ('jawa', 'mamuju', 1), ('sumatra', 'bali', 2), ('sumatra', 'papua', 2), ('sumatra', 'sulawesi', 4), ('bali', 'sulawesi', 3), ('bali', 'kalimantan', 1), ('papua', 'sulawesi', 4), ('papua', 'mamuju', 3), ('sulawesi', 'ternate', 7), ('mamuju', 'ternate', 5), ('ternate', 'kalimantan', 6)]
@@ -79,25 +87,25 @@ def grafberbobotberpasangan():
                 if (hasil[i][j] == hasil[j][i]):
                     g += [(simpul[i], simpul[j], int(hasil[i][j]))]
                 else:
-                    continue;
+                    continue
             else:
-                continue;
+                continue
 
 #Algoritma Utama
-namafile = input("Masukkan file graf : " );
+namafile = input("Masukkan file graf : " )
 readfile(namafile)
 
 ceksimpul()
-#print(simpul)
+#print("simpul:", simpul)
 
 isimatriks()
-#print(hasil)
+#print("hasil:", hasil)
 
 grafindict()
-#print(grafberbobot)
+print(grafberbobot)
 
 grafberbobotberpasangan()
-#print(g)
+print("g:", g)
 
 #simpulasal = input("Masukkan simpul asal : ")
 #simpultujuan = input("Masukkan simpul tujuan : ")
